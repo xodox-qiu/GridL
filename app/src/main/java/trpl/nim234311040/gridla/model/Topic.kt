93ac60eb70cc6fa109cc81e9efc53f62e6c53a0d
xodox-qiu <dvalinnsimulacrum@gmail.com>
2024-12-04 19:07:48 +0700

    Commited 234311040

---
+++ app/src/main/java/trpl/nim234311040/gridla/model/Topic.kt
@@ -0,0 +1,10 @@
+package trpl.nim234311040.gridla.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
